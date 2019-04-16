8eabf5d4149b5d65098cde7671718188196d08cb
Evelyn <evelyn@ipsoft.es>
2019-04-16 15:24:23 +0100

    Test passed for text property on note

--- note-model-spec.js
+++ note-model-spec.js
@@ -1,5 +1,14 @@
+console.log("NOTE OBJECT TESTS");
+
 function instantiated() {
   var note = new Note();
   assert.isTrue(note !== undefined);
 };
 instantiated();
+
+function instantiatedWithText() {
+  var textNote = new Note("Yoda is great!");
+  assert.isTrue(textNote.text === "Yoda is great!");
+};
+
+instantiatedWithText();
--- note-model.js
+++ note-model.js
@@ -1,3 +1,3 @@
-function Note() {
-
+function Note(text) {
+  this.text = text;
 };
